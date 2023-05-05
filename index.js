51b2ab6a68e22349a0a39765d908afb2c5a3c10e
Hilgard van Rooyen <hilgardvr@gmail.com>
2023-05-05 07:05:59 +0200

    part3.15

--- index.js
+++ index.js
@@ -21,28 +21,28 @@ app.use(morgan(function (tokens, req, res) {
   ].join(' ')
 }))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     { 
+//       "id": 1,
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": 2,
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": 3,
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": 4,
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
 
 app.get('/', (req, res) => {
     res.send('<h1>Index</h1>')
@@ -85,7 +85,13 @@ app.get('/api/persons/:id', (req, res) => {
 
 app.delete('/api/persons/:id', (req, res) => {
   const id = req.params.id.toString()
-  persons = persons.filter(p => p.id.toString() != id)
+  Person.findByIdAndRemove(id)
+    .then(result => {
+      console.log(`removed ${id}`, result)
+      res.status(204).end()
+    })
+    .catch(err => console.log(err))
+  // persons = persons.filter(p => p.id.toString() != id)
   res.status(204).end()
 })
 
